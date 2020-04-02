0dcee533d467c7c320ba39d93075bb2015fa6b96
Saunak Sen <sen@uthsc.edu>
2020-04-01 22:23:12 -0500

    Added documentation of analysis functions.

--- src/distancing.jl
+++ src/distancing.jl
@@ -33,31 +33,47 @@ function infectCitizen!(citizenStatus::DataFrame)
     citizenStatus[1,2]=1
 end
 
+```
+updateStatus!(status::DataFrame,distances::Matrix{Float64},
+             time::Int64,infectionDist::Float64,infectionWindow::Int64,
+             infectionProb::Float64)
 
-function updateStatus!(citizens::DataFrame,distances::Matrix{Float64},
-        time::Int64,infectionDist::Float64=0.2,
-        infectionWindow::Int64=1,infectionProb::Float64=0.2)
-    whoCanInfect = citizens.ind[(citizens.infected.>0).&((time.-citizens.infected).<=infectionWindow)]
-    whoSusceptible = citizens.ind[citizens.infected.==0]
+Updates the infection `status` data frame at a given `time` using information
+on `infectionDist` (radius of neighborhood which a person can infect),
+`infectionWindow` (how long an infected person is infectious), `infectionProb`
+(the probability of infecting a susceptible person in a given time period).
+
+The infection model can be tweaked to make it more realistic.
+```
+function updateStatus!(status::DataFrame,distances::Matrix{Float64},
+        time::Int64,infectionDist::Float64,
+        infectionWindow::Int64,infectionProb::Float64)
+
+    # individuals who are still infectious
+    whoCanInfect = status.ind[(status.infected.>0).&
+                   ((time.-status.infected).<=infectionWindow)]
+    # individuals who are susceptible
+    whoSusceptible = status.ind[status.infected.==0]
+    # which individuals can infect whom
     transmissionPossible = (distances[whoCanInfect,whoSusceptible].<=infectionDist)
+    # calculate probability of infection for each person in a given time period
     prob = 1.0.-(1.0.-infectionProb).^(sum(transmissionPossible,dims=1))
-    # citizens.infected[whoSusceptible[rand.(Bernoulli.(prob))]] = time
-    citizens.infected[citizens.ind[whoSusceptible[rand.(Bernoulli.(prob))[1,:]]]] .= time
+    # randomly decide based on infection probability, who is newly infected
+    status.infected[status.ind[whoSusceptible[rand.(Bernoulli.(prob))[1,:]]]] .= time
 end
 
-function f(x)
-    if(x==true)
-        return 0
-    else
-        return 1
-    end
-end
+```
+followCitizens!(citizens::DataFrame,distances::Matrix{Float64},ntime::Int64=365,
+                        infectionDist::Float64,
+                        infectionWindow::Int64,infectionProb::Float64)
 
-# -
-
-function followCitizens!(citizens::DataFrame,distances::Matrix{Float64},ntime::Int64=300,
+Follow the citizens in the data drame `citizens` for `ntime` time periods assuming
+`infectionDist`, `infectionWindow` an `infectionProb` as in updateStatus function.
+```
+function followCitizens!(citizens::DataFrame,distances::Matrix{Float64},ntime::Int64=365,
                         infectionDist::Float64=0.1,
                         infectionWindow::Int64=1,infectionProb::Float64=0.2)
+    # print expected number of infections per infected individual
     print(round(size(citizens,1)*pi*infectionDist^2*infectionWindow*infectionProb,digits=2))
     for i in 2:ntime
          updateStatus!(citizens,distances,i,infectionDist,
@@ -65,49 +81,56 @@ function followCitizens!(citizens::DataFrame,distances::Matrix{Float64},ntime::I
     end
 end
 
+###
+## convert trues to 1
+###
+function trueone(x)
+    if(x==true)
+        return 0
+    else
+        return 1
+    end
+end
+
+###################################################
+
+
+```
+uninfected(status::DataFrame)
+
+Returns number uninfected at end of follow up period.
+```
 function uninfected(status::DataFrame)
     return mean(status.infected.>0)
 end
 
+```
+newInfections(status::DataFrame)
 
-# +
+Make histogram of new infections.
+```
 function newInfections(status::DataFrame)
     histogram(status.infected,bins=range(1,maximum(status.infected),step=1))
 end
 
+```
+cumulativeInfections(status::DataFrame)
+
+Plot cumulative number of infection as a proportion of the population.
+```
 function cumulativeInfections(status::DataFrame)
     plot(1:365,cumsum(counts(status.infected,1:365))./size(status,1),ylim=(0,1))
 end
 
+```
+infectionSpread(status::DataFrame,pos::Matrix{Float64})
+
+Make a animation of the spread of the infection using the `status` data and
+spatial positions in `pos`.
+```
 function infectionSpread(status::DataFrame,pos::Matrix{Float64})
     spread = @animate for i in 1:365
       scatter(pos[:,1],pos[:,2],color=f.((status.infected.<=i).&(status.infected.>0)))
     end
     return spread
 end
-# -
-
-n=10000
-status,d,pos = makeCitizens(n);
-infectCitizen!(status)
-followCitizens!(status,d,365,0.05,14,0.0025)
-
-uninfected(status)
-
-newInfections(status)
-
-cumulativeInfections(status)
-
-gif(infectionSpread(status,pos),fps=15)
-
-n=10000
-status1,d,pos = makeCitizens(n);
-infectCitizen!(status1)
-followCitizens!(status1,d,365,0.05,14,0.002)
-uninfected(status1)
-
-newInfections(status1)
-
-cumulativeInfections(status1)
-
-gif(infectionSpread(status1,pos),fps=15)
