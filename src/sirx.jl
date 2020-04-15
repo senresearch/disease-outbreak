af3fd77602b5605dd65a7f3df962a87098d6ca7a
Saunak Sen <sen@uthsc.edu>
2020-04-15 10:48:05 -0500

    First attempt at SIRX.  Need checking.

---
+++ src/sirx.jl
@@ -0,0 +1,35 @@
+include("seir.jl")
+
+struct SIRXDynamics
+    α::Float64
+    β::Vector{Float64}
+    κ::Float64
+    κ0::Float64
+end
+
+# change in a day
+"""
+change(s::Vector{Float64},d::SIRXDynamics)
+
+Retutn the change in the state of the population in a day
+
+s = state vector (S,I,R)
+d = SIRX dynamics parameters
+"""
+function change(s::Vector{Float64},d::SEI3RDynamics)
+    # this is done purely for readability of the formula
+    st = NamedTuple{(:S,:I,:R,:X)}(s)
+    N = sum(s) # population size
+    S = -(( d.α * st.I + d.κ0)/N) *st.S
+    I = d.α*st.I*st.S/N - d.β*st.I - d.κ0*st.I - d.κ*st.I
+    R = d.β*st.I + d.κ0*st.S/N
+    X = (d.κ+d.κ0)*st.I
+    return [S,E,I,R,X]
+end
+
+function nstates(d::SIRXDynamics)
+    return 4
+end
+function stateNames(d::SIRXDynamics)
+    return ["S" "I" "R" "X"]
+end
