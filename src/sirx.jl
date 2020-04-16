8c2a3a0d3e49a724cdbf579b974a730668e4921d
Saunak Sen <sen@uthsc.edu>
2020-04-16 08:58:52 -0500

    Removed subscripts for better readability.

--- src/sirx.jl
+++ src/sirx.jl
@@ -5,10 +5,10 @@ struct SIRXDynamics
     κ0::Float64
 end
 
-function initialize(N::Float64,C₀::Float64,IXRatio::Float64,d::SIRXDynamics)
+function initialize(N::Float64,C0::Float64,IXRatio::Float64,d::SIRXDynamics)
     state = zeros(nstates(d))
-    state[4] = C₀/N # X0, page 1, Supplement
-    state[2] = IXRatio*(C₀/N) # I0, page 1, supplement
+    state[4] = C0/N # X0, page 1, Supplement
+    state[2] = IXRatio*(C0/N) # I0, page 1, supplement
     state[1] = 1.0-state[2]-state[4]
     return state
 end
@@ -39,9 +39,9 @@ function stateNames(d::SIRXDynamics)
     return ["S" "I" "R" "X"]
 end
 
-function getParams(κ::Float64,κ₀::Float64,
-    R₀Free::Float64,TInfected::Float64)
+function getParams(κ::Float64,κ0::Float64,
+    R0Free::Float64,TInfected::Float64)
     β = 1.0/TInfected
-    α = R₀Free*β
-    return SIRXDynamics(α,β,κ,κ₀)
+    α = R0Free*β
+    return SIRXDynamics(α,β,κ,κ0)
 end
