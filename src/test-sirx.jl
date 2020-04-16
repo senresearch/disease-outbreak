cc1cadbfedbfd71616237b33e728e84184b872aa
Saunak Sen <sen@uthsc.edu>
2020-04-16 09:36:08 -0500

    First attempt at modeling Hubei.

--- src/test-sirx.jl
+++ src/test-sirx.jl
@@ -1,1 +1,9 @@
 include("sirx.jl")
+include("evolution.jl")
+
+# Hubei
+N=57.1e6
+d = getParams(0.038,0.073,6.2,8.0)
+s0 = initialize(N,10.0^3,0.001,d)
+(s,ds) = evolve(N,s0,d,100);
+plotEvolution(N,s,log10=true)
