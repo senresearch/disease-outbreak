b9c6761d8d05c972dd240df2b9acffa7bc50ee0d
Saunak Sen <sen@uthsc.edu>
2020-04-17 09:42:16 -0500

    Using default SIRX constructor.

--- src/test-sirx.jl
+++ src/test-sirx.jl
@@ -3,7 +3,7 @@ include("evolution.jl")
 
 # Hubei
 N=57.1e6
-d = getParams(0.038,0.073,6.2,8.0)
+d = getParams(0.038,0.073,6.2,8.0,SIRX())
 s0 = initialize(N,0.7*10^3,2.55,d)
 (s,ds) = evolve(N,s0,d,100);
 plotEvolution(N,s,d,log10=true)
