ae60991df6ca423c68f348832adce35edcff814d
Saunak Sen <sen@uthsc.edu>
2020-04-17 19:05:23 -0500

    Read in Hubei data and try to reproduce fig in supplement.

--- scripts/test-sirx.jl
+++ scripts/test-sirx.jl
@@ -1,11 +1,17 @@
-include("sirx.jl")
-include("evolution.jl")
+include("../src/sirx.jl")
+include("../src/evolution.jl")
 
 # Hubei
+using CSV
+f = joinpath(@__DIR__, "..", "data", "covid19_china/time_series_covid19_confirmed_hubei.csv")
+
+hubei = CSV.read(f)
 N=57.1e6
 d = getParams(0.038,0.073,6.2,8.0,SIRX())
-s0 = initialize(N,0.7*10^3,2.55,d)
+s0 = initialize(N,hubei.ConfirmedCases[1]*1.0,2.55,d)
 (s,ds) = evolve(N,s0,d,100);
 plotEvolution(N,s,d,log10=true)
 
-plot(N*s'[:,4],yaxis=:log,xaxis=:log,ylim=[10, 10^5],label="")
+plot(N*s'[:,4],yaxis=:log,xaxis=:log,ylim=[10, 10^5],label="fitted")
+plot!((hubei.ConfirmedCases),xaxis=:log,yaxis=:log,label="actual")
+# savefig("hubei.pdf")
