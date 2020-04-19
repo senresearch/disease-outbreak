d72deb610c34a951bd7b5659c6e75f6c9908efa0
Saunak Sen <sen@uthsc.edu>
2020-04-19 13:15:43 -0500

    Working functions; new type caseModelFitResult.

--- scripts/test-sirx.jl
+++ scripts/test-sirx.jl
@@ -1,28 +1,29 @@
+using LsqFit
+using CSV
 include("../src/sirx.jl")
 include("../src/evolution.jl")
-
 # Hubei
-using CSV
 hubeiFile = joinpath(@__DIR__, "..", "data",
      "covid19_china","time_series_covid19_confirmed_hubei.csv")
 
 hubei = CSV.read(hubeiFile)
-hubeiPop=57.1e6
 d = getParams(0.038,0.073,6.2,8.0,SIRX())
-s0 = initialize(hubeiPop,hubei.ConfirmedCases[1]*1.0,2.55,d)
-(s,ds) = evolve(hubeiPop,s0,d,100);
-plotEvolution(hubeiPop,s,d,log10=true)
+hubeiPop = SIRXPopulation(57.0e6,hubei.ConfirmedCases[1]*1.0,2.55)
+s0 = initialize(hubeiPop,d)
+(s,ds) = evolve(hubeiPop.N,s0,d,100);
+plotEvolution(hubeiPop.N,s,d,log10=true)
 
+t = convert(Vector{Float64},(1:length(hubei.ConfirmedCases)))
+y = convert(Vector{Float64},hubei.ConfirmedCases)
 
-plot(hubeiPop*s'[:,4],yaxis=:log,xaxis=:log,
+hubeiPop.N
+fit = fitCaseModel(t,y,hubeiPop.N,6.2,8.0,[0.1,0.1,2.0])
+summary(fit)
+
+plot(hubeiPop.N*s'[:,4],yaxis=:log,xaxis=:log,
      ylim=[10, 10^5],label="fitted")
 plot!((hubei.ConfirmedCases),xaxis=:log,yaxis=:log,label="actual")
 plot!(caseModel(x,exp.(fit.param),hubeiPop,
                        hubei.ConfirmedCases[1]*1.0,
                        6.2,8.0),yaxis=:log, label="estimated")
 # savefig("hubei.pdf")
-
-g(p) = ss(y,x,p)
-git = optimize(g,log.([0.1,0.1,1.0]),NelderMead())
-
-exp.(git.minimizer)
--- src/sirx.jl
+++ src/sirx.jl
@@ -43,7 +43,7 @@ end
 function initialize(pop::SIRXPopulation,d::SIRX)
     state = zeros(nstates(d))
     state[4] = pop.C0/pop.N # X0, page 1, Supplement
-    state[2] = IXRatio*(pop.C0/pop.N) # I0, page 1, supplement
+    state[2] = pop.IXRatio*(pop.C0/pop.N) # I0, page 1, supplement
     state[1] = 1.0-state[2]-state[4] # S0
     return state
 end
@@ -76,23 +76,43 @@ Returns vector of cases given model dynamics and population parameters.
 function caseModel(t::Vector{Float64},pop::SIRXPopulation,d::SIRX)
     s0 = initialize(pop,d)
     (d,ds) =evolve(pop.N,s0,d,length(t))
-    return N.*d[4,:]
+    return pop.N.*d[4,:]
 end
 
 function logCaseModelUnknown(t::Vector{Float64},p::Vector{Float64},
     N::Float64,C0::Float64,R0Free::Float64,TInfected::Float64)
     d = getParams(p[1],p[2],R0Free,TInfected,SIRX())
-    pop = SIRXPopulation(N,C0,p[3],d)
-    return log.(caseModel(t,pop,d)
+    pop = SIRXPopulation(N,C0,p[3])
+    return log.(caseModel(t,pop,d))
 end
 
-function rss(y::Vector{Float64},x::Vector{Float64},p::Vector{Float64})
-    return sum((y-model(x,p)).^2)
+# function rss(y::Vector{Float64},x::Vector{Float64},p::Vector{Float64})
+#    return sum((y-model(x,p)).^2)
+# end
+
+struct CaseModelFitResult
+    κ::Float64
+    κ0::Float64
+    IXRatio::Float64
+    fit::LsqFit.LsqFitResult
+    inputs::NamedTuple
 end
 
-x = convert(Vector{Float64},(1:length(hubei.ConfirmedCases)))
-y = log.(hubei.ConfirmedCases)
-fit = curve_fit(model,x,y,[log(0.1),log(0.1),log(1)])
+function fitCaseModel(t::Vector{Float64},cases::Vector{Float64},
+    N::Float64,R0Free::Float64,TInfected::Float64,
+    p0::Vector{Float64})
+    inputs = NamedTuple{(:N,:R0Free,:TInfected)}([N,R0Free,TInfected])
+    model(t,p) = logCaseModelUnknown(t,exp.(p),N,cases[1],R0Free,TInfected)
+    fit = curve_fit(model,t,log.(cases),log.(p0))
+    return CaseModelFitResult( exp(fit.param[1]), exp(fit.param[2]),
+                         exp(fit.param[3]), fit, inputs )
+end
 
-fit
-exp.(fit.param)
+function R0Eff(d::SIRX)
+    return d.α/(d.β+d.κ+d.κ0)
+end
+
+function summary(fit::CaseModelFitResult)
+    r0eff = R0Eff(getParams(fit.κ,fit.κ0,fit.inputs.R0Free,fit.inputs.TInfected,SIRX()))
+    res = return NamedTuple{(:κ,:κ0,:IXRatio,:R0Eff)}([fit.κ,fit.κ0,fit.IXRatio,r0eff])
+end
